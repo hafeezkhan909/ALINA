c158ca69b7333d35cdcf6940b6eb15ee991a18e8
hafeezkhan909 <f20190091@dubai.bits-pilani.ac.in>
2023-02-06 11:15:44 -0500

    In this main file, all the functions are called algorithmically.
    1. Load the image using the imread method of OpenCV, which reads the image from a file and returns it as a matrix.
    2. Convert the image to grayscale using the cvtColor method of OpenCV.
    3. Use the "auto_canny" function to detect the edges in the grayscale image.
    4. Get the coordinates of the pixels with the value 255 (which correspond to edges) using the "np.where" method.
    5. Simplify the edges using the Ramer-Douglas-Peucker algorithm and store the simplified coordinates in "simplified_coords".
    6. Use the Hough transform to detect lines in the edge image.
    7. Draw the lines and the simplified edges on the original image using the "cv2.line" and "cv2.circle" methods.
    8. Display the image with lines using "cv2.imshow".
    9. Crop the image using the y-coordinate of the first line.
    10. Train a KMeans model using color and texture features of the original image.
    11. Use the trained KMeans model to extract the road from the cropped and original images.
    12. Display the results using the "cv2.imshow" method.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,69 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+from recursiveAlgorithm import recursive_algo
+from automatedCanny import auto_canny
+from extraction import normalize_color_features, normalize_texture_features, extract_road
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+file = "/home/hafeez/Desktop/cropped_images/00052.jpg"
+img = cv2.imread(file)
+cv2.imshow("original Image", img)
+print(img.shape)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+rgb2 = cv2.imread(file)
+rgb3 = cv2.imread(file)
+rgb4 = cv2.imread(file)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+edges = auto_canny(gray)
+rows, cols = np.where(edges == 255)
+print(rows)
+print(cols)
+coords = np.column_stack((cols, rows))
+print(coords)
+
+# Simplify the edges using the Ramer-Douglas-Peucker algorithm
+threshold = 0.1  # adjust as needed
+simplified_coords = np.array(recursive_algo(coords, threshold))
+print(simplified_coords)
+
+# Find lines in the simplified edges using the Hough transform
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=30, maxLineGap=10)
+
+# Draw the lines on the image
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+# Draw the simplified edges on the image
+for coord in simplified_coords:
+    pixel_color = rgb2[coord[1], coord[0]]
+    cv2.circle(img, (coord[0], coord[1]), 1, (0, 0, 255), 2)
+
+# Show the image
+cv2.imshow("Image with lines", img)
+
+y = lines[0][0][1]
+cropped_image = rgb2[y:, :]
+cv2.imshow("cropped image", cropped_image)
+print(cropped_image.shape)
+
+# Train KMeans model
+kmeans = KMeans(n_clusters=3, init='k-means++', n_init=10, max_iter=500, tol=0.0001, verbose=0, random_state=None, copy_x=True)
+color_features = normalize_color_features(rgb3).reshape(-1, 3)
+texture_features = normalize_texture_features(rgb3).reshape(-1, 3)
+multi_features = np.concatenate((color_features, texture_features), axis=-1)
+kmeans.fit(multi_features)
+
+# Extract the road from the image
+road1 = extract_road(cropped_image, kmeans)
+road2 = extract_road(rgb4, kmeans)
+
+# Display the result
+cv2.imshow('Result1', road1)
+cv2.imshow('Result2', road2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
