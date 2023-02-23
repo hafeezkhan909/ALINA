ab1f348f819a62e05aeb5062fa07f8666d82bba6
hafeezkhan909 <f20190091@dubai.bits-pilani.ac.in>
2023-02-23 15:15:01 -0500

    Combine features is a new function where the color, texture and gradient are combined together using a simple weighted sum.

--- src/r.py
+++ src/r.py
@@ -1,15 +1,9 @@
 import cv2
 import numpy as np
 from sklearn.cluster import KMeans
-
-def combine_features(color_features, texture_features, gradient_features, w1, w2, w3):
-
-    combined_features = w1 * color_features + w2 * texture_features + w3 * gradient_features
-    combination_features = combined_features.astype(np.uint8)
-    cv2.imshow('MMMMMMM', combination_features)
-    return combined_features
-
+import os
 def recursive_algo(points, threshold):
+
     dmax = 0
     index = 0
     for i in range(1, len(points) - 1):
@@ -19,13 +13,12 @@ def recursive_algo(points, threshold):
             dmax = d
 
     if dmax > threshold:
-        rec_results1 = recursive_algo(points[:index + 1], threshold)
+        rec_results1 = recursive_algo(points[:index+1], threshold)
         rec_results2 = recursive_algo(points[index:], threshold)
 
         return rec_results1[:-1] + rec_results2
     else:
         return [points[0], points[-1]]
-
 
 def dist_to_segment(p, v, w):
     """
@@ -38,13 +31,11 @@ def dist_to_segment(p, v, w):
     projection = v + t * (w - v)
     return dist_sq(p, projection)
 
-
 def dist_sq(p1, p2):
     """
     Squared Euclidean distance between two points p1 and p2.
     """
     return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
-
 
 def auto_canny(image, sigma=0.33):
     intensity = np.median(image)
@@ -53,16 +44,14 @@ def auto_canny(image, sigma=0.33):
     edge = cv2.Canny(img, lower, higher)
     return edge
 
-
 def build_filters():
     filters = []
     ksize = 31
     for theta in np.arange(0, np.pi, np.pi / 16):
         kern = cv2.getGaborKernel((ksize, ksize), 4.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
-        kern /= 1.5 * kern.sum()
+        kern /= 1.5*kern.sum()
         filters.append(kern)
     return filters
-
 
 def process(img, filters):
     accum = np.zeros_like(img)
@@ -70,7 +59,6 @@ def process(img, filters):
         fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
         np.maximum(accum, fimg, accum)
     return accum
-
 
 def normalize_color_features(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -82,7 +70,6 @@ def normalize_color_features(image):
     cv2.imshow("CNormalized", color_features)
     return color_features
 
-
 def normalize_texture_features(image):
     filters = build_filters()
     texture_features = process(image, filters)
@@ -90,22 +77,20 @@ def normalize_texture_features(image):
     cv2.imshow("TNormalized", texture_features)
     return texture_features
 
-
 def gradient_normalization(image):
     gradient_x = cv2.Sobel(image, cv2.CV_32F, 1, 0, ksize=3)
     gradient_y = cv2.Sobel(image, cv2.CV_32F, 0, 1, ksize=3)
-    gradient_magnitude = np.sqrt(gradient_x ** 2 + gradient_y ** 2)
+    gradient_magnitude = np.sqrt(gradient_x**2 + gradient_y**2)
     gradient_magnitude = gradient_magnitude / gradient_magnitude.max()
     cv2.imshow("GjfjfjNormalized", gradient_magnitude)
     return gradient_magnitude
-
 
 def extract_road(image, kmeans_model):
     color_features = normalize_color_features(image)
     texture_features = normalize_texture_features(image)
     gradient_features = gradient_normalization(image)
-    multi_features = combine_features(color_features, texture_features, gradient_features, 0.3, 0.5, 0.2)
-    multi_features = multi_features.reshape(-1, 3)
+    multi_features = np.concatenate((color_features, texture_features, gradient_features), axis=-1)
+    multi_features = multi_features.reshape(-1, 9)
     print(multi_features)
     labels = kmeans_model.predict(multi_features)
     labels = labels.reshape(*image.shape[:2])
@@ -117,7 +102,6 @@ def extract_road(image, kmeans_model):
         cluster_mask = (labels == k)
         color_img[cluster_mask] = colors[k]
     return color_img
-
 
 file = "/home/hafeez/Desktop/cropped_images/10159.jpg"
 img = cv2.imread(file)
@@ -142,7 +126,7 @@ simplified_coords = np.array(recursive_algo(coords, threshold))
 print(simplified_coords)
 
 # Find lines in the simplified edges using the Hough transform
-lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, minLineLength=30, maxLineGap=10)
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=30, maxLineGap=10)
 
 # Draw the lines on the image
 if lines is not None:
@@ -164,19 +148,21 @@ cv2.imshow("cropped image", cropped_image)
 print(cropped_image.shape)
 
 # Train KMeans model
-kmeans = KMeans(n_clusters=9, init='k-means++', n_init=10, max_iter=5000, tol=0.001, verbose=0, random_state=None, copy_x=True)
+kmeans = KMeans(n_clusters=9, init='k-means++', n_init=10, max_iter=5000, tol=0.1, verbose=0, random_state=None, copy_x=True)
 color_features = normalize_color_features(rgb3).reshape(-1, 3)
 texture_features = normalize_texture_features(rgb3).reshape(-1, 3)
 gradient_features = gradient_normalization(rgb3).reshape(-1, 3)
-multi_features = combine_features(color_features, texture_features, gradient_features, 0.3, 0.5, 0.2)
+multi_features = np.concatenate((color_features, texture_features, gradient_features), axis=-1)
+cv2.imshow('MMMMMMM', multi_features)
 kmeans.fit(multi_features)
 
 # Extract the road from the image
 road1 = extract_road(cropped_image, kmeans)
 road2 = extract_road(rgb4, kmeans)
-
+#output_dir = "/home/hafeez/Desktop"
 # Display the result
 cv2.imshow('Result1', road1)
+#cv2.imwrite(os.path.join(output_dir, 'MMM4.jpg'), road2)
 cv2.imshow('Result2', road2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
