7321984c87340fef01e40196bb18dfe9c4aeadcb
hafeezkhan909 <f20190091@dubai.bits-pilani.ac.in>
2023-03-09 17:01:46 -0500

    After making the change in the r.py, the lower and upper yellow thresholds have been determined for HSV feature space in order to threshold the yellow pixel line markings from the rest of the image.

--- src/lines.py
+++ src/lines.py
@@ -11,22 +11,39 @@ cv2.waitKey()
 # Convert to HSV color space
 #hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 #cv2.imshow('HSV', hsv)
+
 #cv2.waitKey()
 
+# C
 # Define the lower and upper ranges of yellow hue values
-lower_yellow = np.array([60, 90, 0]) # lower hue value of yellow
+lower_yellow = np.array([0, 70, 150]) # lower hue value of yellow
 upper_yellow = np.array([255, 255, 255]) # upper hue value of yellow
 
+# T + C + G
+# lower_yellow = np.array([40, 65, 0]) # lower hue value of yellow
+# upper_yellow = np.array([120, 110, 120]) # upper hue value of yellow
 
 
 # Create a binary mask
 mask = cv2.inRange(img, lower_yellow, upper_yellow)
 cv2.imshow('Mask', mask)
-cv2.imwrite('/home/hafeez/Desktop/features.jpg', mask)
+print(mask.shape)
+
+# Create mask
+masked = np.ones(img.shape[:2], dtype=np.uint8)
+masked[:, 0:300] = 0
+
+# Apply mask to image
+masked_img = cv2.bitwise_and(mask, mask, mask=masked)
+
+# Display masked image
+cv2.imshow('Masked Image', masked_img)
+
+cv2.imwrite('/home/hafeez/Desktop/features.jpg', masked_img)
 cv2.waitKey()
 # Apply the mask to the original image to extract yellow pixels
 yellow_pixels = cv2.bitwise_and(warped, warped, mask=mask)
-
+#cv2.imwrite('/home/hafeez/Desktop/features.jpg', yellow_pixels)
 
 # Display the result
 cv2.imshow('Yellow Pixels', yellow_pixels)
