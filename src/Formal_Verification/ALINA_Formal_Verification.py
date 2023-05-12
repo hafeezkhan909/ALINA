68d2d209665e17657486e55868d97f6665140773
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2023-05-12 15:50:07 -0400

    Added precision function

--- src/Formal_Verification/ALINA_Formal_Verification.py
+++ src/Formal_Verification/ALINA_Formal_Verification.py
@@ -1,58 +1,83 @@
 import numpy as np
 import os
 
-def calculate_iou(coords1, coords2):
-    set1 = set(coords1)
-    set2 = set(coords2)
-    intersection = set1 & set2
-    inter = set1 & set2
-    total = inter | set2
-    union = set1 | set2
-    iou = (len(intersection) / len(total))*100
-    return iou
+def calculate_recall(coords1, coords2):
+   set1 = set(coords1)
+   set2 = set(coords2)
+   TP = set1 & set2
+   FN = set1 - set2
+   recall = (len(TP) / (len(TP) + len(FN))) * 100
+   return recall
+
+def calculate_precision(coords1, coords2):
+   set1 = set(coords1)
+   set2 = set(coords2)
+   TP = set1 & set2
+   FP = set2 - set1
+   precision = (len(TP) / (len(TP) + len(FP))) * 100
+   return precision
 
 # Directories containing text files
-canny_dir = r'C:\Users\assist-lab\Desktop\FV\canny_textfiles'
-alina_dir = r'C:\Users\assist-lab\Desktop\FV\ALINA_textfiles2'
+canny_dirs = [
+   r'C:\Users\assist-lab\Desktop\FV\canny_textfiles\canny_textfiles_1',
+   r'C:\Users\assist-lab\Desktop\FV\canny_textfiles\canny_textfiles_2',
+   r'C:\Users\assist-lab\Desktop\FV\canny_textfiles\canny_textfiles_3',
+]
+
+alina_dirs = [
+   r'C:\Users\assist-lab\Desktop\FV\ALINA_textfiles\ALINA_textfiles_1',
+   r'C:\Users\assist-lab\Desktop\FV\ALINA_textfiles\ALINA_textfiles_2',
+   r'C:\Users\assist-lab\Desktop\FV\ALINA_textfiles\ALINA_textfiles_3',
+]
 
 # Loop through each text file in the directory
-iou_values = []
-for filename in os.listdir(canny_dir):
-    if filename.endswith('.txt'):
-        # Load x and y coordinates from Canny text file
-        with open(os.path.join(canny_dir, filename)) as f:
-            lines = f.readlines()
+iterator = 0
+recall_values = []
+precision_values = []
+for dir in canny_dirs:
+   for filename in os.listdir(dir):
+       if filename.endswith('.txt'):
+           # Load x and y coordinates from Canny text file
+           with open(os.path.join(dir, filename)) as f:
+               lines = f.readlines()
 
-        # Extract x and y coordinates and convert to numpy arrays
-        x1 = np.zeros((len(lines),), dtype=np.int32)
-        y1 = np.zeros((len(lines),), dtype=np.int32)
-        for i, line in enumerate(lines):
-            x, y = line.strip().split()
-            x1[i] = int(x)
-            y1[i] = int(y)
+           # Extract x and y coordinates and convert to numpy arrays
+           x1 = np.zeros((len(lines),), dtype=np.int32)
+           y1 = np.zeros((len(lines),), dtype=np.int32)
+           for i, line in enumerate(lines):
+               x, y = line.strip().split()
+               x1[i] = int(x)
+               y1[i] = int(y)
 
-        # Load x and y coordinates from Alina text file
-        with open(os.path.join(alina_dir, filename)) as f:
-            lines = f.readlines()
+           # Load x and y coordinates from Alina text file
+           with open(os.path.join(alina_dirs[iterator], filename)) as f:
+               lines = f.readlines()
 
-        # Extract x and y coordinates and convert to numpy arrays
-        x2 = np.zeros((len(lines),), dtype=np.int32)
-        y2 = np.zeros((len(lines),), dtype=np.int32)
-        for i, line in enumerate(lines):
-            x, y = line.strip().split()
-            x2[i] = int(x)
-            y2[i] = int(y)
+           # Extract x and y coordinates and convert to numpy arrays
+           x2 = np.zeros((len(lines),), dtype=np.int32)
+           y2 = np.zeros((len(lines),), dtype=np.int32)
+           for i, line in enumerate(lines):
+               x, y = line.strip().split()
+               x2[i] = int(x)
+               y2[i] = int(y)
 
-        # Calculate IoU over x-coordinates
-        iou_x = calculate_iou(x1, x2)
+           # Calculate IoU over x-coordinates
+           recall_x = calculate_recall(x1, x2)
+           precision_x = calculate_precision(x1, x2)
+           # Calculate IoU over y-coordinates
+           recall_y = calculate_recall(y1, y2)
+           precision_y = calculate_precision(y1, y2)
+           # Combine the two results
+           recall = (recall_x + recall_y) / 2
+           precision = (precision_x + precision_y) / 2
+           recall_values.append(recall)
+           precision_values.append(precision)
+   iterator += 1
 
-        # Calculate IoU over y-coordinates
-        iou_y = calculate_iou(y1, y2)
+# Print the average recall over all text files
+avg_recall = np.mean(recall_values)
+print("Average Recall:", avg_recall, '%')
 
-        # Combine the two results
-        iou = (iou_x + iou_y) / 2
-        iou_values.append(iou)
-
-# Print the average IoU over all text files
-avg_iou = np.mean(iou_values)
-print("Average IoU:", avg_iou, '%')
+# Print the average precision over all text files
+avg_precision = np.mean(precision_values)
+print("Average Precision:", avg_precision, '%')
