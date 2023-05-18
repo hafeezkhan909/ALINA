f5001088fb5c9633bf611a1925e1b2c4d14c2a21
Mohammed Abdul Hafeez Khan <82383549+hafeezkhan909@users.noreply.github.com>
2023-05-18 12:10:08 -0400

    Added the Interactive User functionality

--- src/ALINA_src_code/ALINA.py
+++ src/ALINA_src_code/ALINA.py
@@ -3,35 +3,36 @@ import numpy as np
 from ColorFeatureNormalization import normalize_color_features
 from HistogramAnalysis import calc_histogram
 from CIRCLEDAT_Optimized import circular_threshold_pixel_discovery_and_traversal
+from interactiveUser import select_roi
 from datetime import datetime
 import os
 
 # Set the input directory, where the images reside
-input_directory = r"C:\Users\assist-lab\Desktop\bad"
+input_directory = r"C:\Users\assist-lab\Desktop\r"
 
 # Get list of image filenames in ascending order
 image_filenames = sorted([f for f in os.listdir(input_directory) if f.endswith('.jpg')])
 
 # Set the output directory, where the images and their corresponding text files should be saved
-output_directory1 = r"C:\Users\assist-lab\Desktop\vidd_annotations_4"
-output_directory2 = "C:\\Users\\assist-lab\\Desktop\\vidd_textfile_4\\"
+output_directory1 = r"C:\Users\assist-lab\Desktop\v1"
+output_directory2 = "C:\\Users\\assist-lab\\Desktop\\v2\\"
 
 
-def detect_and_extract_taxiway_line_pixels(img):
+def detect_and_extract_taxiway_line_pixels(img, roi_points):
 
     final_img = img.copy()
     img1 = img.copy()
     no_lines_img = img.copy()
 
-    roi1 = np.array([[(640, 780), (590, 700),
-                      (1160, 700), (1110, 780)]],
-                       dtype=np.int32)
+    roi1 = np.array([[(roi_points[0][0][0], roi_points[0][0][1]), (roi_points[0][1][0], roi_points[0][1][1]),
+                  (roi_points[0][3][0], roi_points[0][1][1]), (roi_points[0][5][0], roi_points[0][0][1])]],
+                   dtype=np.int32)
 
     cv2.polylines(img1, roi1, True, (0, 0, 255), thickness=2)
 
     # Define the region of interest as a trapezoid
-    roi = np.array([[(640, 780), (590, 700),
-                     (1160, 700), (1110, 780)]],
+    roi = np.array([[(roi_points[0][0][0], roi_points[0][0][1]), (roi_points[0][1][0], roi_points[0][1][1]),
+                  (roi_points[0][3][0], roi_points[0][1][1]), (roi_points[0][5][0], roi_points[0][0][1])]],
                       dtype=np.float32)
 
     # Define the desired rectangular shape
@@ -51,7 +52,7 @@ def detect_and_extract_taxiway_line_pixels(img):
     img1 = cv2.imread(r"C:\Users\assist-lab\Desktop\combined_features.jpg")
 
     # Define the lower and upper ranges of yellow hue values
-    lower_yellow = np.array([0, 70, 170]) # lower hue value of yellow
+    lower_yellow = np.array([0, 70, 160]) # lower hue value of yellow
     upper_yellow = np.array([255, 255, 255]) # upper hue value of yellow
 
     # Create a binary mask
@@ -76,7 +77,7 @@ def detect_and_extract_taxiway_line_pixels(img):
 
     print(peak_value)
 
-    if (peak_value > 75):
+    if (peak_value > 50):
 
         # Define the circular threshold
         threshold = 15
@@ -152,12 +153,34 @@ def time_calc():
 
 
 # Open the file for writing
-with open(r"C:\Users\assist-lab\Desktop\vid_time_calc\processing_times_vidd_40.txt", 'w') as f:
+with open(r"C:\Users\assist-lab\Desktop\vid_time_calc\processing_times_vidd_rr.txt", 'w') as f:
+
+    # Giving the initial image to user for drawing the trapezoid
+    print('Hello User, Welcome to ALINA!\n'
+          'Please draw the inverse trapezoid on the image for specifying the ROI. There are some things you should keep in mind while drawing:\n'
+          '1. The order of 4 points for drawing the inverse trapezoidal ROI: Bottom Left, Top Left, Top Right, Bottom Right, Bottom Left\n'
+          '2. Do not click more than once on any point.\n'
+          '3. Once you complete selecting the 4 points, press any key on your keyboard to visualize the trapezoid (in corrected form)\n'
+          '4. If you are satisfied with the trapezoid, again press any key on the keyboard to begin ALINA, else stop the program and run it again')
+
+    roi_points = select_roi(os.path.join(input_directory, image_filenames[0]))
+
+    reference_roi = np.array([[(roi_points[0][0][0], roi_points[0][0][1]), (roi_points[0][1][0], roi_points[0][1][1]),
+                            (roi_points[0][3][0], roi_points[0][1][1]), (roi_points[0][5][0], roi_points[0][0][1])]],
+                            dtype=np.int32)
+
+    reference_image = cv2.imread(os.path.join(input_directory, image_filenames[0]))
+
+    cv2.polylines(reference_image, reference_roi, True, (0, 0, 255), thickness=2)
+    cv2.imshow('Polyline1234', reference_image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
     for filename in image_filenames:
         if filename.endswith(".jpg"):
             start_time = datetime.now().strftime("%H:%M:%S")
             # Load the image
             img = cv2.imread(os.path.join(input_directory, filename))
-            detect_and_extract_taxiway_line_pixels(img)
+            detect_and_extract_taxiway_line_pixels(img, roi_points)
 
 cv2.destroyAllWindows()
